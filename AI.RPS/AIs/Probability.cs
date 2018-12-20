7fdc57dd19d90bb2f7e55671e047516e9e9eba3e
Coco <constantin.lungu1@yahoo.com>
2018-12-20 23:35:11 +0200

    Update functie probabilitate
    Functia de probabilitate ia in calcul si ce a mai jucat si user-ul inainte.

--- AI.RPS/AIs/Probability.cs
+++ AI.RPS/AIs/Probability.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using AI.RPS.Interfaces;
 
@@ -8,12 +9,16 @@ namespace AI.RPS.AIs
     {
         private static readonly Random Random = new Random(Guid.NewGuid().GetHashCode());
         private double RockProbability, PaperProbability, ScissorsProbability;
-        private int strategy = 0;
+        private double numberOfRock = 0, numberOfPaper = 0, numberOfscissors = 0, total = 0;
 
         public Choice Run(Game game)
         {
             if (game.Round == 0)
+            {
+                if (UserExists(game.Name))
+                    GetProbabilitiesFromFile(game);
                 return (Choice)Random.Next(3);
+            }
 
             GetProbabilities(game);
 
@@ -22,20 +27,54 @@ namespace AI.RPS.AIs
 
         public void GetProbabilities(Game game)
         {
-            double numberOfRock = 0, numberOfPaper = 0, numberOfscissors = 0, total = 0;
-
-            foreach (var round in game.History.Rounds)
+            switch (game.History.Rounds.Last().PlayerChoice)
             {
-                switch (round.PlayerChoice)
+                case Choice.Scissors:
+                    { numberOfscissors++; total++; break; }
+                case Choice.Rock:
+                    { numberOfRock++; total++; break; }
+                case Choice.Paper:
+                    { numberOfPaper++; total++; break; }
+                default:
+                    { total++; break; }
+            }
+
+            SetProbabiltyRock(numberOfRock / total);
+            SetProbabiltyPaper(numberOfPaper / total);
+            SetProbabiltyScissors(numberOfscissors / total);
+        }
+
+        public void GetProbabilitiesFromFile(Game game)
+        {
+            var fileName = "../../Users/" + game.Name + ".txt";
+            string[] allLines;
+            bool firstLine = true;
+            allLines = File.ReadAllLines(fileName);
+
+            foreach (var line in allLines)
+            {
+                if (firstLine)
                 {
-                    case Choice.Scissors:
-                        { numberOfscissors++; total++; break; }
-                    case Choice.Rock:
-                        { numberOfRock++; total++; break; }
-                    case Choice.Paper:
-                        { numberOfPaper++; total++; break; }
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(round.PlayerChoice), round.PlayerChoice, "WTF?!");
+                    firstLine = false;
+                    continue;
+                }
+
+                if (line.Equals("Scissors"))
+                {
+                    numberOfscissors++;
+                    total++;
+                }
+
+                if (line.Equals("Rock"))
+                {
+                    numberOfRock++;
+                    total++;
+                }
+
+                if (line.Equals("Paper"))
+                {
+                    numberOfPaper++;
+                    total++;
                 }
             }
 
@@ -43,6 +82,7 @@ namespace AI.RPS.AIs
             SetProbabiltyPaper(numberOfPaper / total);
             SetProbabiltyScissors(numberOfscissors / total);
         }
+
 
         public void SetProbabiltyRock(double number)
         {
@@ -62,6 +102,8 @@ namespace AI.RPS.AIs
         public Choice Max()
         {
             double result = Math.Max(RockProbability, Math.Max(PaperProbability, ScissorsProbability));
+            /*Console.WriteLine("Nr Rock: " + numberOfRock + " Nr Paper: " + numberOfPaper + " Nr Scissors: " + numberOfscissors + " Total: " + total);
+            Console.WriteLine("Rock: " + RockProbability + " Paper: " + PaperProbability + " Scissors: " + ScissorsProbability);*/
 
             if (result == RockProbability)
                 return Choice.Paper;
@@ -70,6 +112,16 @@ namespace AI.RPS.AIs
                 return Choice.Scissors;
 
             return Choice.Rock;
+        }
+
+        public bool UserExists(string name)
+        {
+            var fileName = "../../Users/" + name + ".txt";
+
+            if (File.Exists(fileName))
+                return true;
+
+            return false;
         }
     }
 }
