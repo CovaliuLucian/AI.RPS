d9957839a96b4a9c6576ed8b2dcfdbf33917ba7c
Coco <constantin.lungu1@yahoo.com>
2018-12-21 01:26:33 +0200

    Strategie mixta

---
+++ AI.RPS/AIs/Mixed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AI.RPS.Interfaces;
+
+namespace AI.RPS.AIs
+{
+    public class Mixed : IArtificialInteligence
+    {
+        private RandomPicker Random = new RandomPicker();
+        private Probability Probability = new Probability();
+        private static readonly Random Rand = new Random(Guid.NewGuid().GetHashCode());
+
+        public Choice Run(Game game)
+        {
+            if (game.Round == 0)
+            {
+                Probability.Run(game);
+                return Random.Run(game);
+            }
+
+            switch(Rand.Next(2))
+            {
+                case 0:
+                    { return Probability.Run(game); }
+                default:
+                    { Probability.Run(game); return Random.Run(game); }
+            }
+            
+        }
+    }
+}
