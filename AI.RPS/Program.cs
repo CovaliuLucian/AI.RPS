3e8e0b663698d7b1fd4d788d854bd96968ff9e53
Coco <constantin.lungu1@yahoo.com>
2018-12-21 01:27:06 +0200

    Nivele de dificultate

--- AI.RPS/Program.cs
+++ AI.RPS/Program.cs
@@ -7,8 +7,60 @@ namespace AI.RPS
     {
         public static void Main(string[] args)
         {
-            var result = GameRunner.Run(new Probability());
-            Console.ReadLine();
+            var difficulty = ChooseDifficulty();
+            Score result;
+
+            switch(difficulty)
+            {
+                case "easy":
+                    result = GameRunner.Run(new Probability());
+                    break;
+                case "medium":
+                    result = GameRunner.Run(new Mixed());
+                    break;
+                default:
+                    result = GameRunner.Run(new RandomPicker());
+                    break;
+            }
+        }
+
+        private static void FancyWrite(string @string)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@string);
+            Console.ResetColor();
+        }
+
+        public static string ChooseDifficulty()
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose the difficulty: ");
+                FancyWrite("1. Easy");
+                FancyWrite("2. Medium");
+                FancyWrite("3. Hard");
+                var difficulty = Console.ReadLine();
+
+                switch (difficulty.ToLower())
+                {
+                    case "1":
+                    case "e":
+                    case "easy":
+                        return "easy";
+                    case "2":
+                    case "m":
+                    case "medium":
+                        return "medium";
+                    case "3":
+                    case "h":
+                    case "hard":
+                        return "hard";
+                    default:
+                        Console.WriteLine("Wrong input, try again.");
+                        break;
+
+                }
+            }
         }
     }
 }
